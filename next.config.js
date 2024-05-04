14e4838f599d88b675fb80061893558b5c419a07
testefrain <testefrain@ssh1.paris1.alwaysdata.com>
2024-05-04 06:08:22 +0200

    Primer commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: '',
+  reactStrictMode: true,
+  experimental: {
+    appDir: false
+  }
+}
+
+module.exports = nextConfig
